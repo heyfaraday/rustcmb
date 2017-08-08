37c523678f8184a3df20f520b163fc390241d5e9
Ivan Edigarev <heyfaraday@gmail.com>
2017-08-08 16:19:17 +0300

    diff initial and new modules in lib

---
+++ src/diff/mod.rs
@@ -0,0 +1,29 @@
+fn d_1d(field: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+
+    let size: usize = field.capacity() - 1;
+    assert!(field.capacity() == size + 1);
+
+    let mut answer: Vec<Vec<f64>> = vec![vec![0.; size + 1]; size + 1];
+
+    answer
+}
+
+fn d_2d_x(field: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+
+    let size: usize = field.capacity() - 1;
+    assert!(field.capacity() == size + 1);
+
+    let mut answer: Vec<Vec<f64>> = vec![vec![0.; size + 1]; size + 1];
+
+    answer
+}
+
+fn d_2d_y(field: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+
+    let size: usize = field.capacity() - 1;
+    assert!(field.capacity() == size + 1);
+
+    let mut answer: Vec<Vec<f64>> = vec![vec![0.; size + 1]; size + 1];
+
+    answer
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+pub mod corr;
+pub mod diff;
 pub mod fourier;
 pub mod io;
+pub mod math;
 pub mod spectra;
