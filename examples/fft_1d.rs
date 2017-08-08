3bc0f65e9155e4715ac864342db3dfdcbca94da9
Ivan Edigarev <heyfaraday@gmail.com>
2017-08-08 17:26:58 +0300

    1d fft

--- examples/fft_1d.rs
+++ examples/fft_1d.rs
@@ -1,9 +1,9 @@
 extern crate rustcmb;
 
-use rustcmb::fourier::{fft_1d_first_realization, fft_1d_second_realization,
-                       fft_1d_third_realization};
+use rustcmb::fourier::fft_1d;
 use rustcmb::io::write_1d;
 
+#[allow(dead_code)]
 const DATA_IN: &str = "data/examples/int/fft_1d/";
 const DATA_OUT: &str = "data/examples/out/fft_1d/";
 const SIZE: usize = 8;
@@ -14,26 +14,33 @@ fn main() {
     let mut a_mods: Vec<f64> = vec![0.; SIZE / 2 + 1];
     let mut b_mods: Vec<f64> = vec![0.; SIZE / 2 + 1];
 
+    a_mods[0] = 1.;
+    a_mods[1] = 2.;
+    a_mods[2] = 123.;
+    a_mods[3] = 3.;
+    a_mods[4] = 5.;
+    // b_mods[0] = 1.;
+    b_mods[1] = 100.;
+    b_mods[2] = 223.;
+    b_mods[3] = 34.;
+    // b_mods[4] = 100.;
+
     println!("simple_field: {:?}", simple_field);
     println!("a_mods: {:?}", a_mods);
     println!("b_mods: {:?}", b_mods);
 
-    a_mods[0] = 1.;
-    a_mods[1] = 2.;
-    a_mods[2] = 3.;
-    a_mods[4] = -4.;
-    b_mods[1] = 1.;
-    b_mods[2] = 3.;
-    b_mods[3] = -5.;
-
-    fft_1d_first_realization(&mut simple_field, &a_mods, &b_mods);
+    fft_1d::first_realization(&mut simple_field, &a_mods, &b_mods);
     println!("simple_field after first: {:?}", simple_field);
 
-    fft_1d_second_realization(&mut simple_field, &a_mods, &b_mods);
+    fft_1d::second_realization(&mut simple_field, &a_mods, &b_mods);
     println!("simple_field after second: {:?}", simple_field);
 
-    fft_1d_third_realization(&mut simple_field, &a_mods, &b_mods);
+    fft_1d::third_realization(&mut simple_field, &a_mods, &b_mods);
     println!("simple_field after third: {:?}", simple_field);
 
     write_1d(&simple_field, &DATA_OUT, &"f.dat");
+
+    fft_1d::back(&simple_field, &mut a_mods, &mut b_mods);
+    println!("a_mods: {:?}", a_mods);
+    println!("b_mods: {:?}", b_mods);
 }
---
+++ src/fourier/fft_1d.rs
@@ -0,0 +1,122 @@
+extern crate rustfft;
+
+use self::rustfft::algorithm::Radix4;
+use self::rustfft::FFT;
+use self::rustfft::num_complex::Complex;
+use self::rustfft::num_traits::Zero;
+
+use std::f64::consts::PI;
+
+pub fn first_realization(field: &mut Vec<f64>, a_mods: &Vec<f64>, b_mods: &Vec<f64>) {
+
+    let size: usize = field.capacity() - 1;
+
+    assert!(field.capacity() == size + 1);
+    assert!(a_mods.capacity() == size / 2 + 1);
+    assert!(b_mods.capacity() == size / 2 + 1);
+    assert!(b_mods[0] == 0.);
+    assert!(b_mods[size / 2] == 0.);
+
+    let mut input: Vec<Complex<f64>> = vec![Complex::zero(); size];
+    let mut output: Vec<Complex<f64>> = vec![Complex::zero(); size];
+
+    for i in 0..(size / 2 + 1) {
+        input[i] = Complex::new(a_mods[i], 0.);
+    }
+
+    let fft = Radix4::new(size, false);
+    fft.process(&mut input, &mut output);
+
+    for i in 0..(size) {
+        field[i] = output[i].re;
+    }
+    field[size] = output[0].re;
+
+    for i in 1..(size / 2) {
+        input[i] = Complex::new(b_mods[i], 0.);
+    }
+
+    let fft = Radix4::new(size, false);
+    fft.process(&mut input, &mut output);
+
+    for i in 0..(size) {
+        field[i] -= output[i].im;
+    }
+    field[size] -= output[0].im;
+}
+
+pub fn second_realization(field: &mut Vec<f64>, a_mods: &Vec<f64>, b_mods: &Vec<f64>) {
+
+    let size: usize = field.capacity() - 1;
+
+    assert!(field.capacity() == size + 1);
+    assert!(a_mods.capacity() == size / 2 + 1);
+    assert!(b_mods.capacity() == size / 2 + 1);
+    assert!(b_mods[0] == 0.);
+    assert!(b_mods[size / 2] == 0.);
+
+    let mut input: Vec<Complex<f64>> = vec![Complex::zero(); size];
+    let mut output: Vec<Complex<f64>> = vec![Complex::zero(); size];
+
+    for i in 0..(size / 2 + 1) {
+        input[i] = Complex::new(a_mods[i], b_mods[i]);
+    }
+
+    let fft = Radix4::new(size, false);
+    fft.process(&mut input, &mut output);
+
+    for i in 0..(size) {
+        field[i] = output[i].re;
+    }
+    field[size] = output[0].re;
+}
+
+pub fn third_realization(field: &mut Vec<f64>, a_mods: &Vec<f64>, b_mods: &Vec<f64>) {
+
+    let size: usize = field.capacity() - 1;
+
+    assert!(field.capacity() == size + 1);
+    assert!(a_mods.capacity() == size / 2 + 1);
+    assert!(b_mods.capacity() == size / 2 + 1);
+    assert!(b_mods[0] == 0.);
+    assert!(b_mods[size / 2] == 0.);
+
+    let h = (2. * PI) / (size as f64);
+
+    for i in 0..(size + 1) {
+        field[i] = 0.;
+        for i_mode in 0..(size / 2 + 1) {
+
+            let arg = (i * i_mode) as f64;
+            field[i] += a_mods[i_mode] * (h * arg).cos() + b_mods[i_mode] * (h * arg).sin();
+        }
+    }
+}
+
+pub fn back(field: &Vec<f64>, a_mods: &mut Vec<f64>, b_mods: &mut Vec<f64>) {
+
+    let size = field.capacity() - 1;
+
+    assert!(field.capacity() == size + 1);
+    assert!(a_mods.capacity() == size / 2 + 1);
+    assert!(b_mods.capacity() == size / 2 + 1);
+    assert!(b_mods[0] == 0.);
+    assert!(b_mods[size / 2] == 0.);
+
+    let mut input: Vec<Complex<f64>> = vec![Complex::zero(); size];
+    let mut output: Vec<Complex<f64>> = vec![Complex::zero(); size];
+
+    for i in 0..(size) {
+        input[i] = Complex::new(field[i], 0.);
+    }
+
+    let fft = Radix4::new(size, true);
+    fft.process(&mut input, &mut output);
+
+    a_mods[0] = output[0].re / 8.;
+    a_mods[size / 2] = output[size / 2].re / 8.;
+    for i in 1..(size / 2) {
+        a_mods[i] = output[i].re / 4.;
+        b_mods[i] = output[i].im / 4.;
+    }
+}
