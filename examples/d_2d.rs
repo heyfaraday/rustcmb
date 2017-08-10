af2dfcd8d1f217836ada9ac1c6fddb01f8828478
Ivan Edigarev <heyfaraday@gmail.com>
2017-08-11 02:12:35 +0300

    d_2d example added and py for it

---
+++ examples/d_2d.rs
@@ -0,0 +1,38 @@
+extern crate rustcmb;
+
+use rustcmb::fourier::fft_2d;
+use rustcmb::diff::d_2d::*;
+use rustcmb::io::write_2d;
+use rustcmb::spectra::gasdev;
+
+#[allow(dead_code)]
+const DATA_IN: &str = "data/examples/in/d_2d/";
+const DATA_OUT: &str = "data/examples/out/d_2d/";
+const SIZE: usize = 8;
+
+fn main() {
+
+    let mut field: Vec<Vec<f64>> = vec![vec![0.; SIZE + 1]; SIZE + 1];
+    let mut a_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
+    let mut b_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
+
+
+    gasdev(&field, &mut a_mods, &mut b_mods, 0., 1.);
+
+    fft_2d::first_realization(&mut field, &a_mods, &b_mods);
+    write_2d(&field, &DATA_OUT, &"field.dat");
+
+    let mut field_xx: Vec<Vec<f64>> = vec![vec![0.; SIZE + 1]; SIZE + 1];
+    let mut field_yy: Vec<Vec<f64>> = vec![vec![0.; SIZE + 1]; SIZE + 1];
+    let mut field_answ: Vec<Vec<f64>> = vec![vec![0.; SIZE + 1]; SIZE + 1];
+
+    d_2d_xx(&mut field_xx, &a_mods, &b_mods);
+    d_2d_yy(&mut field_yy, &a_mods, &b_mods);
+
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            field_answ[i][j] = field_xx[i][j] + field_yy[i][j];
+        }
+    }
+    write_2d(&field_answ, &DATA_OUT, &"field_laplace.dat");
+}
---
+++ py/examples/d_2d/image.py
@@ -0,0 +1,20 @@
+from numpy import *
+from pylab import *
+
+size = 8
+
+plt.figure(figsize=(8, 8))
+x, y, z = genfromtxt("../../../data/examples/out/d_2d/field.dat").T
+x = x.reshape(size + 1, size + 1)
+y = y.reshape(size + 1, size + 1)
+z = z.reshape(size + 1, size + 1)
+pcolor(x, y, z)
+show()
+
+plt.figure(figsize=(8, 8))
+x, y, z = genfromtxt("../../../data/examples/out/d_2d/field_laplace.dat").T
+x = x.reshape(size + 1, size + 1)
+y = y.reshape(size + 1, size + 1)
+z = z.reshape(size + 1, size + 1)
+pcolor(x, y, z)
+show()
