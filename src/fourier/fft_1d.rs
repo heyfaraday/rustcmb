4864ae97f61e9e5beabbc182a67c9cc145b5bdc4
Ivan Edigarev <heyfaraday@gmail.com>
2017-08-10 14:21:13 +0300

    1d fft small fix

--- src/fourier/fft_1d.rs
+++ src/fourier/fft_1d.rs
@@ -95,7 +95,7 @@ pub fn third_realization(field: &mut Vec<f64>, a_mods: &Vec<f64>, b_mods: &Vec<f
 
 pub fn back(field: &Vec<f64>, a_mods: &mut Vec<f64>, b_mods: &mut Vec<f64>) {
 
-    let size = field.capacity() - 1;
+    let size: usize = field.capacity() - 1;
 
     assert!(field.capacity() == size + 1);
     assert!(a_mods.capacity() == size / 2 + 1);
@@ -113,10 +113,10 @@ pub fn back(field: &Vec<f64>, a_mods: &mut Vec<f64>, b_mods: &mut Vec<f64>) {
     let fft = Radix4::new(size, true);
     fft.process(&mut input, &mut output);
 
-    a_mods[0] = output[0].re / 8.;
-    a_mods[size / 2] = output[size / 2].re / 8.;
+    a_mods[0] = output[0].re / (size as f64);
+    a_mods[size / 2] = output[size / 2].re / (size as f64);
     for i in 1..(size / 2) {
-        a_mods[i] = output[i].re / 4.;
-        b_mods[i] = output[i].im / 4.;
+        a_mods[i] = output[i].re / (size as f64) * 2.;
+        b_mods[i] = output[i].im / (size as f64) * 2.;
     }
 }
