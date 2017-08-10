c4a3500a851521509b4a70b9c7bb5ca69be15871
Ivan Edigarev <heyfaraday@gmail.com>
2017-08-11 02:38:40 +0300

    tensor and vector field test added

---
+++ examples/tensor_field.rs
@@ -0,0 +1,45 @@
+extern crate rustcmb;
+
+use rustcmb::corr::correlation_function;
+use rustcmb::spectra::{gasdev_max_k, gasdev_exp_k0};
+use rustcmb::fourier::fft_2d::first_realization;
+use rustcmb::diff::d_2d::{d_2d_xx, d_2d_yy, d_2d_xy};
+use rustcmb::io::write_1d;
+
+const SIZE: usize = 64;
+const DATA_OUT: &str = "data/examples/out/tensor_field/";
+const MAX_ARG: f64 = SIZE as f64 / 4.;
+const SCALE: usize = 20;
+
+fn main() {
+
+    let mut field: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    let mut a_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
+    let mut b_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
+
+    gasdev_max_k(&field, &mut a_mods, &mut b_mods, 0., 1., 4.);
+    gasdev_exp_k0(&field, &mut a_mods, &mut b_mods, 0., 1., MAX_ARG); // or
+
+    first_realization(&mut field, &a_mods, &b_mods);
+
+    let corr_function = correlation_function(&field, SCALE);
+    write_1d(&corr_function[0], DATA_OUT, "corr_function.dat");
+    write_1d(&corr_function[1], DATA_OUT, "norm_for_corr_function.dat");
+
+    let mut field_xx: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    let mut field_yy: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    let mut field_xy: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    d_2d_xx(&mut field_xx, &a_mods, &b_mods);
+    d_2d_yy(&mut field_yy, &a_mods, &b_mods);
+    d_2d_xy(&mut field_xy, &a_mods, &b_mods);
+
+    let mut q: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    let mut u: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            q[i][j] = field_xx[i][j] - field_yy[i][j];
+            u[i][j] = 2. * field_xy[i][j];
+        }
+    }
+}
---
+++ examples/vector_field.rs
@@ -0,0 +1,45 @@
+extern crate rustcmb;
+
+use rustcmb::corr::{correlation_function, correlation_function_vector_field};
+use rustcmb::spectra::{gasdev_max_k, gasdev_exp_k0};
+use rustcmb::fourier::fft_2d::first_realization;
+use rustcmb::diff::d_2d::{d_2d_x, d_2d_y};
+use rustcmb::io::write_1d;
+
+const SIZE: usize = 64;
+const DATA_OUT: &str = "data/examples/out/vector_field/";
+const MAX_ARG: f64 = SIZE as f64 / 4.;
+const SCALE: usize = 20;
+
+fn main() {
+
+    let mut field: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    let mut a_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
+    let mut b_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
+
+    gasdev_max_k(&field, &mut a_mods, &mut b_mods, 0., 1., 4.);
+    gasdev_exp_k0(&field, &mut a_mods, &mut b_mods, 0., 1., MAX_ARG); // or
+
+    first_realization(&mut field, &a_mods, &b_mods);
+
+    let corr_function = correlation_function(&field, SCALE);
+    write_1d(&corr_function[0], DATA_OUT, "corr_function.dat");
+    write_1d(&corr_function[1], DATA_OUT, "norm_for_corr_function.dat");
+
+    let mut field_x: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    let mut field_y: Vec<Vec<f64>> = vec![vec![1.; SIZE + 1]; SIZE + 1];
+    d_2d_x(&mut field_x, &a_mods, &b_mods);
+    d_2d_y(&mut field_y, &a_mods, &b_mods);
+
+    let corr_function_vector_field = correlation_function_vector_field(&field_x, &field_y, SCALE);
+    write_1d(
+        &corr_function_vector_field[0],
+        DATA_OUT,
+        "corr_function_vector_field.dat",
+    );
+    write_1d(
+        &corr_function_vector_field[1],
+        DATA_OUT,
+        "norm_for_corr_function_vector_field.dat",
+    );
+}
