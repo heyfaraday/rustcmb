223dbaa809185e540e0102e269a23c21d308319b
Ivan Edigarev <edigaryev.ig@phystech.edu>
2018-03-05 21:45:21 +0300

    Small fix for d_2d example

--- examples/d_2d.rs
+++ examples/d_2d.rs
@@ -3,7 +3,7 @@ extern crate rustcmb;
 use rustcmb::fourier::fft_2d;
 use rustcmb::diff::d_2d::*;
 use rustcmb::io::write_2d;
-use rustcmb::spectra::gasdev;
+use rustcmb::spectra::gasdev_max_k;
 
 #[allow(dead_code)]
 const DATA_IN: &str = "data/examples/in/d_2d/";
@@ -16,8 +16,7 @@ fn main() {
     let mut a_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
     let mut b_mods: Vec<Vec<f64>> = vec![vec![0.; SIZE]; SIZE / 2 + 1];
 
-
-    gasdev(&field, &mut a_mods, &mut b_mods, 0., 1.);
+    gasdev_max_k(&field, &mut a_mods, &mut b_mods, 0., 1., 1.);
 
     fft_2d::torus::first_realization(&mut field, &a_mods, &b_mods);
     write_2d(&field, &DATA_OUT, &"field.dat");
@@ -29,9 +28,9 @@ fn main() {
     d_2d_xx(&mut field_xx, &a_mods, &b_mods);
     d_2d_yy(&mut field_yy, &a_mods, &b_mods);
 
-    for i in 0..SIZE {
-        for j in 0..SIZE {
-            field_answ[i][j] = field_xx[i][j] + field_yy[i][j];
+    for i in 0..(SIZE + 1) {
+        for j in 0..(SIZE + 1) {
+            field_answ[i][j] = - field_xx[i][j] - field_yy[i][j];
         }
     }
     write_2d(&field_answ, &DATA_OUT, &"field_laplace.dat");
