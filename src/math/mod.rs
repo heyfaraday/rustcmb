66e4dc0ea79f956f052d5102cdb9923e3ec5c2fe
Ivan Edigarev <edigaryev.ig@phystech.edu>
2018-04-11 14:43:06 +0300

    torus_distance_square method and small fixes

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -9,7 +9,7 @@ use err::point_assert;
 use std::f64::consts::PI;
 
 pub fn torus_distance(field: &Vec<Vec<f64>>, point_1: &[usize; 2], point_2: &[usize; 2]) -> f64 {
-    let size = field.capacity() - 1;
+    let size = field.len() - 1;
 
     point_assert(&field, &point_1);
     point_assert(&field, &point_2);
@@ -39,6 +39,39 @@ pub fn torus_distance(field: &Vec<Vec<f64>>, point_1: &[usize; 2], point_2: &[us
     let arg: f64 = i * i + j * j;
 
     arg.sqrt()
+}
+
+pub fn torus_distance_square(field: &Vec<Vec<f64>>, point_1: &[usize; 2], point_2: &[usize; 2]) -> f64 {
+    let size = field.len() - 1;
+
+    point_assert(&field, &point_1);
+    point_assert(&field, &point_2);
+
+    let i: f64 = if point_1[0] >= point_2[0] {
+        point_1[0] as f64 - point_2[0] as f64
+    } else {
+        point_2[0] as f64 - point_1[0] as f64
+    };
+    let j: f64 = if point_1[1] >= point_2[1] {
+        point_1[1] as f64 - point_2[1] as f64
+    } else {
+        point_2[1] as f64 - point_1[1] as f64
+    };
+
+    let i: f64 = if i > (size as f64) / 2. {
+        size as f64 - i
+    } else {
+        i
+    };
+    let j: f64 = if j > (size as f64) / 2. {
+        size as f64 - j
+    } else {
+        j
+    };
+
+    let arg: f64 = i * i + j * j;
+
+    arg
 }
 
 pub fn angle_for_vector_field(vx: &f64, vy: &f64) -> f64 {
@@ -98,7 +131,7 @@ pub fn j_mode_normalized(size: usize, j: usize) -> i32 {
 }
 
 pub fn fourier_distance(field: &Vec<Vec<f64>>, k1: usize, k2: usize) -> f64 {
-    let size = field.capacity() - 1;
+    let size = field.len() - 1;
 
     size_assert_2d_for_field(&field);
     assert!(k1 < (size / 2 + 1));
